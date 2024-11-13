cd71ce09b4d2eb66369447f9aecdb1fc42b3f96f
. <.>
2024-11-13 10:46:44 +0700

    penis and balls

---
+++ remove_aur_cache.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+CACHE_DIR=$XDG_CACHE_HOME/sexy_aur_helper
+
+rm $CACHE_DIR -d
---
+++ update_aur.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Exit on errors and stuff, idk read the manual
+set -Eeuo pipefail
+
+if [ -z "$XDG_CACHE_HOME" ]; then
+  echo '$XDG_CACHE_HOME is empty!'
+  exit
+fi
+
+CACHE_DIR=$XDG_CACHE_HOME/sexy_aur_helper
+
+if [ ! -d "$CACHE_DIR" ]; then
+  echo "Creating $CACHE_DIR"
+  mkdir "$CACHE_DIR"
+fi
+
+cd $CACHE_DIR
+
+AUR_PKGS="$(pacman -Qemq)"
+
+for pkg in $AUR_PKGS; do
+  echo $pkg
+done
