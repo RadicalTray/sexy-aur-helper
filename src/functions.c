7b151aadfab19695a008b0376ac49fbda19cd332
RadicalTray <hydrochloricmahn@gmail.com>
2024-11-14 20:34:41 +0700

    remove not needed

--- src/functions.c
+++ src/functions.c
@@ -3,21 +3,6 @@
 #include "types.h"
 #include "globals.h"
 
-Opts process_args(const int len, char **args) {
-    Opts opts = g_default_opts;
-    for (int i = 0; i < len; i++) {
-        for (const char *p_char = args[i]; *p_char != 0; p_char++) {
-        }
-    }
-    return opts;
-}
-
-void process_opt() {
-}
-
-void process_long_opt() {
-}
-
 void print_help() {
     printf("HELP\n");
 }
--- src/functions.h
+++ src/functions.h
@@ -1,12 +1,9 @@
 #pragma once
 #include "types.h"
 
-Opts process_args(const int len, char **args);
 int run_search(const int len, const char **args);
 int run_sync(const int len, const char **args);
 int run_upgrade(const int len, const char **args);
 int run_update_pkg_list(const int len, const char **args);
 int run_clear_cache(const int len, const char **args);
-void process_opt();
-void process_long_opt();
 void print_help();
