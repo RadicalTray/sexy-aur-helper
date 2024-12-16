4311bf2bf1c6ecdb8a8175f619590de0777d2d12
RadicalTray <radicaltray@gmail.com>
2024-12-16 21:12:26 +0700

    hi

--- src/sync.c
+++ src/sync.c
@@ -7,13 +7,152 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 
+int build_and_install(const int clone_dir_path_len,
+                const char *clone_dir_path,
+                const int makepkg_opts_len,
+                char *const makepkg_opts[],
+                const int sync_pkg_count,
+                const char **sync_pkg_list);
+
+int run_sync(const int len, const char **args) {
+    const char *makepkg_opts_str = "";
+    int sync_pkg_count = 0;
+    bool is_pkg[len] = {}; // hopefully initialized to false
+    for (int i = 0; i < len; i++) {
+        if (strcmp(args[i], "-o") == 0 ||
+            strcmp(args[i], "--options") == 0) {
+            if (i + 1 == len) {
+                fprintf(stderr, "Specify options for makepkg.\n");
+                return 1;
+            }
+            i++;
+            makepkg_opts_str = args[i];
+            continue;
+        }
+
+        sync_pkg_count++;
+        is_pkg[i] = true;
+    }
+
+    if (sync_pkg_count == 0) {
+        fprintf(stderr, "Specify packages to sync.\n");
+        return 1;
+    }
+
+    const char *sync_pkg_list[sync_pkg_count];
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+        if (is_pkg[i]) {
+            sync_pkg_list[j] = args[i];
+            j++;
+            if (j == sync_pkg_count) {
+                break;
+            }
+        }
+    }
+
+    const int makepkg_opts_str_len = strlen(makepkg_opts_str);
+    int makepkg_opts_len = 0;
+
+    // check for number of args
+    bool after_whitespace = true;
+    for (int i = 0; i < makepkg_opts_str_len; i++) {
+        if (makepkg_opts_str[i] == ' ') {
+            after_whitespace = true;
+            continue;
+        }
+        if (after_whitespace) {
+            makepkg_opts_len++;
+            after_whitespace = false;
+        }
+    }
+
+    char *makepkg_opts[makepkg_opts_len];
+    int current_opt_idx = 0;
+    for (int i = 0; i < makepkg_opts_str_len; i++) {
+        if (current_opt_idx == makepkg_opts_len) {
+            break;
+        }
+
+        if (makepkg_opts_str[i] == ' ') {
+            continue;
+        }
+
+        int opt_strlen = 0;
+        for (int j = i; j < makepkg_opts_str_len; j++) {
+            if (makepkg_opts_str[j] == ' ') {
+                break;
+            }
+
+            opt_strlen++;
+        }
+
+        char *opt_str = malloc(opt_strlen + 1);
+        memcpy(opt_str, makepkg_opts_str + i, opt_strlen);
+        opt_str[opt_strlen] = '\0';
+
+        makepkg_opts[current_opt_idx] = opt_str;
+
+        i += opt_strlen;
+        current_opt_idx++;
+    }
+
+    int ret = sync_pkg(sync_pkg_count, sync_pkg_list, makepkg_opts_len, makepkg_opts);
+    for (int i = 0; i < makepkg_opts_len; i++) {
+        free(makepkg_opts[i]);
+    }
+    return ret;
+}
+
+int sync_pkg(const int sync_pkg_count, const char **sync_pkg_list, const int makepkg_opts_len, char *const makepkg_opts[]) {
+    bool error = false;
+    for (int i = 0; i < sync_pkg_count; i++) {
+        int ret = pkg_is_in_aur(strlen(sync_pkg_list[i]), sync_pkg_list[i]);
+        // could've used if else but nvm
+        switch (ret) {
+            case 1: {
+                fprintf(stdout, "'%s' not found in aur package list.\n", sync_pkg_list[i]);
+                error = true;
+                break;
+            } case 69: {
+                error = true;
+                break;
+            }
+        }
+    }
+    if (error) {
+        return 1;
+    }
+
+    const int cache_dir_len = strlen(g_cache_dir);
+    const char *clone_dir = "/clone";
+    const int clone_dir_len = strlen(clone_dir);
+
+    const int clone_dir_path_len = cache_dir_len + clone_dir_len;
+    char clone_dir_path[clone_dir_path_len + 1];
+    memcpy(clone_dir_path, g_cache_dir, cache_dir_len);
+    memcpy(clone_dir_path + cache_dir_len, clone_dir, clone_dir_len + 1);
+
+    int mkdir_err = mkdir(clone_dir_path, S_IRWXU);
+    if (mkdir_err != 0 && errno != EEXIST) {
+        perror("mkdir");
+    }
+
+    return build_and_install(clone_dir_path_len,
+                       clone_dir_path,
+                       makepkg_opts_len,
+                       makepkg_opts,
+                       sync_pkg_count,
+                       sync_pkg_list);
+}
+
 // TODO:
 //  - tell when something (PKGBUILD changes, etc) is changed from the aur
 //
 // TODO: abstract execvp
 //
 // NOTE: not accounting for when directory is bad (empty, deleted something in it, etc)
-int run_makepkg(const int clone_dir_path_len,
+int build_and_install(const int clone_dir_path_len,
                 const char *clone_dir_path,
                 const int makepkg_opts_len,
                 char *const makepkg_opts[],
@@ -124,136 +263,4 @@ int run_makepkg(const int clone_dir_path_len,
     chdir(initial_cwd);
     free(initial_cwd);
     return 0;
-}
-
-int sync_pkg(const int sync_pkg_count, const char **sync_pkg_list, const int makepkg_opts_len, char *const makepkg_opts[]) {
-    bool error = false;
-    for (int i = 0; i < sync_pkg_count; i++) {
-        int ret = pkg_is_in_aur(strlen(sync_pkg_list[i]), sync_pkg_list[i]);
-        // could've used if else but nvm
-        switch (ret) {
-            case 1: {
-                fprintf(stdout, "'%s' not found in aur package list.\n", sync_pkg_list[i]);
-                error = true;
-                break;
-            } case 69: {
-                error = true;
-                break;
-            }
-        }
-    }
-    if (error) {
-        return 1;
-    }
-
-    const int cache_dir_len = strlen(g_cache_dir);
-    const char *clone_dir = "/clone";
-    const int clone_dir_len = strlen(clone_dir);
-
-    const int clone_dir_path_len = cache_dir_len + clone_dir_len;
-    char clone_dir_path[clone_dir_path_len + 1];
-    memcpy(clone_dir_path, g_cache_dir, cache_dir_len);
-    memcpy(clone_dir_path + cache_dir_len, clone_dir, clone_dir_len + 1);
-
-    int mkdir_err = mkdir(clone_dir_path, S_IRWXU);
-    if (mkdir_err != 0 && errno != EEXIST) {
-        perror("mkdir");
-    }
-
-    return run_makepkg(clone_dir_path_len,
-                       clone_dir_path,
-                       makepkg_opts_len,
-                       makepkg_opts,
-                       sync_pkg_count,
-                       sync_pkg_list);
-}
-
-int run_sync(const int len, const char **args) {
-    const char *makepkg_opts_str = "";
-    int sync_pkg_count = 0;
-    bool is_pkg[len] = {}; // hopefully initialized to false
-    for (int i = 0; i < len; i++) {
-        if (strcmp(args[i], "-o") == 0 ||
-            strcmp(args[i], "--options") == 0) {
-            if (i + 1 == len) {
-                fprintf(stderr, "Specify options for makepkg.\n");
-                return 1;
-            }
-            i++;
-            makepkg_opts_str = args[i];
-            continue;
-        }
-
-        sync_pkg_count++;
-        is_pkg[i] = true;
-    }
-
-    if (sync_pkg_count == 0) {
-        fprintf(stderr, "Specify packages to sync.\n");
-        return 1;
-    }
-
-    const char *sync_pkg_list[sync_pkg_count];
-    int j = 0;
-    for (int i = 0; i < len; i++) {
-        if (is_pkg[i]) {
-            sync_pkg_list[j] = args[i];
-            j++;
-            if (j == sync_pkg_count) {
-                break;
-            }
-        }
-    }
-
-    const int makepkg_opts_str_len = strlen(makepkg_opts_str);
-    int makepkg_opts_len = 0;
-
-    // check for number of args
-    bool after_whitespace = true;
-    for (int i = 0; i < makepkg_opts_str_len; i++) {
-        if (makepkg_opts_str[i] == ' ') {
-            after_whitespace = true;
-            continue;
-        }
-        if (after_whitespace) {
-            makepkg_opts_len++;
-            after_whitespace = false;
-        }
-    }
-
-    char *makepkg_opts[makepkg_opts_len];
-    int current_opt_idx = 0;
-    for (int i = 0; i < makepkg_opts_str_len; i++) {
-        if (current_opt_idx == makepkg_opts_len) {
-            break;
-        }
-
-        if (makepkg_opts_str[i] == ' ') {
-            continue;
-        }
-
-        int opt_strlen = 0;
-        for (int j = i; j < makepkg_opts_str_len; j++) {
-            if (makepkg_opts_str[j] == ' ') {
-                break;
-            }
-
-            opt_strlen++;
-        }
-
-        char *opt_str = malloc(opt_strlen + 1);
-        memcpy(opt_str, makepkg_opts_str + i, opt_strlen);
-        opt_str[opt_strlen] = '\0';
-
-        makepkg_opts[current_opt_idx] = opt_str;
-
-        i += opt_strlen;
-        current_opt_idx++;
-    }
-
-    int ret = sync_pkg(sync_pkg_count, sync_pkg_list, makepkg_opts_len, makepkg_opts);
-    for (int i = 0; i < makepkg_opts_len; i++) {
-        free(makepkg_opts[i]);
-    }
-    return ret;
 }
